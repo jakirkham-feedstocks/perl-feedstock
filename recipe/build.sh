8e4a0126473b94a7734e403b301cde353593ef28
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-04-28 12:38:12 -0400

    recipe: Configure `perl` for CPAN.

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,3 +3,10 @@ sh Configure -de -Dprefix=$PREFIX -Duserelocatableinc
 make
 make test
 make install
+
+# Configure CPAN.
+export PERL_MM_USE_DEFAULT=1
+perl -MCPAN -eshell
+
+# Add LWP as this is very useful.
+perl -MCPAN -e 'install Bundle::LWP'
